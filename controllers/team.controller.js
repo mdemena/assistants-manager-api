8bfd78d4e9c3ef8875441ec185891f0691e8e571
mdemena <mdemena@gmail.com>
2021-04-18 11:37:16 +0200

    Finishing Controllers

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -1,10 +1,14 @@
 const Team = require('../models/team.model');
+const ClubController = require('../controllers/club.controller');
+const SeasonController = require('../controllers/season.controller');
+const CategoryController = require('../controllers/category.controller');
 const mongoose = require('mongoose');
 class TeamController {
 	static async get(id) {
-		return await Team.findById(id);
+		return await (await Team.findById(id)).populate(['club','category','season']);
 	}
 	static async set(team) {
+		team.name = TeamController.getName(team.club, team.season, team.category)
 		const editItem = await Team.findByIdAndUpdate(team._id, team, {
 			new: true,
 		});
@@ -14,12 +18,12 @@ class TeamController {
 		const { club, season, category, name } = team;
 		return await TeamController.add(club, season, category, name);
 	}
-	static async add(season, category, name) {
+	static async add(club, season, category, name) {
 		const newItem = await Team.create({
             club,
 			season,
 			category,
-			name
+			name: TeamController.getName(club, season, category)
         });
 		return newItem;
 	}
@@ -37,6 +41,13 @@ class TeamController {
 	}
 	static async list(filter) {
 		return await Team.find(filter).sort('name');
+	}
+	static async getName(club, season, category) {
+		const clubName = ClubController.get(club);
+		const categoryName = CategoryController.get(category);
+		const seasonName = SeasonController.get(season);
+
+		return categoryName + ' ' + clubName + ' ' + seasonName;
 	}
 }
 module.exports = TeamController;
